35da52a24731298aaa198cdc7ba1fe06c68e43d1
srj-17 <saugatrijal17@gmail.com>
2024-08-09 13:52:58 +0545

    Move tictactoe to GUI with some aspects left
    Starting the game, resetting the board is done.
    Logic for winner is also done, except displaying winner
    on the browser window is remaining.
    Also, setting player names different also left.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const startButton = document.querySelector('.start');
 const resetButton = document.createElement('button');
 resetButton.classList.add('reset');
 resetButton.textContent = 'Reset';
+let markerContainers;
 
 /* gameboard first
 ** gameboard is the representation of the state of the board
@@ -27,7 +28,9 @@ let gameboard = (function() {
             board[i] = [];
             for (let j = 0; j < columns; j++) {
                 // cell is an object that we will create later
-                board[i].push(cell());
+                let newCell = cell();
+                newCell.setPosition(3 * i + j + 1);
+                board[i].push(newCell);
             }
         }
         displayController.displayBoard();
@@ -35,18 +38,6 @@ let gameboard = (function() {
 
     function getBoard() {
         return board;
-    }
-
-    function getBoardMarkers() {
-        let marks = [];
-        board.forEach(row => {
-            let rowMarks = [];
-            row.forEach(cell => {
-                rowMarks.push(cell.getMark());
-            })
-            marks.push(rowMarks);
-        });
-        return marks;
     }
 
     function resetBoard() {
@@ -59,24 +50,47 @@ let gameboard = (function() {
         displayController.displayBoard();
     }
 
-    return {getBoard, getBoardMarkers, createBoard, resetBoard};
+    let positions = [];
+    for (let i = 0; i < 3; i++) {
+        let row = [];
+        for (let j = 0; j < 3; j++) {
+            row.push(3 * i + j + 1);
+        }
+        positions.push(row);
+    } 
+
+    function getPositions() {
+        return positions;
+    }
+
+    return {getBoard, createBoard, resetBoard, getPositions};
 }) ();
 
 // cell stores the state of the particular cell
 function cell() {
     let mark = null;
-    setMark = (myMark) => {
+    let position = null;
+    let setMark = (myMark) => {
         if (!mark) {
             mark = myMark;
         }
     }
-    resetMark = () => {
+    
+    let resetMark = () => {
         mark = null;
     }
-    getMark = () => mark;
-    isMarked = () => mark ? true : false;
+    
+    let setPosition = (x) => {
+        position = x;
+    }
+    
+    let getPosition = () => position;
 
-    return {getMark, setMark, isMarked, resetMark};
+    let getMark = () => mark;
+    
+    let isMarked = () => mark ? true : false;
+
+    return {getMark, setMark, isMarked, resetMark, setPosition, getPosition};
 }
 
 // player should be a new object controlled by gameController but not accessible by anyone else
@@ -111,30 +125,8 @@ let displayController = (function() {
                     markerContainer.textContent = cell.getMark();
                 }
                 ticTacBoard.appendChild(markerContainer);
-                
-                console.log(cell.getMark());
             })
         });
-    }
-
-    // only for console version
-    let positions = [];
-    for (let i = 0; i < 3; i++) {
-        let row = [];
-        for (let j = 0; j < 3; j++) {
-            row.push(3 * i + j + 1);
-        }
-        positions.push(row);
-    } 
-
-    function displayPositions() {
-        positions.forEach(row => {
-            console.log(row);
-        });
-    }
-
-    function getPositions() {
-        return positions;
     }
 
     // swaps the start and reset buttons
@@ -143,13 +135,14 @@ let displayController = (function() {
     }
 
     // now, changing these functions to display in the DOM
-    return {displayBoard, displayPositions, getPositions, swapButtons};
+    return {displayBoard, swapButtons};
 }) ();
 
 let gameController = (function() {
     let playerOne = createPlayer("PlayerOne", "x");
     let playerTwo = createPlayer("PlayerTwo", "o");
     let activePlayer = playerOne;    
+    let board = gameboard.getBoard();
 
     function changeActivePlayer() {
         activePlayer = (activePlayer === playerOne) ? playerTwo : playerOne;
@@ -157,7 +150,13 @@ let gameController = (function() {
 
     function checkWinner() {
         let marker = activePlayer.getMarker();
-        let boardMarkers = gameboard.getBoardMarkers();
+        let boardMarkers = [];
+        board.forEach((row, i) => {
+            boardMarkers[i] = [];
+            row.forEach(cell => {
+                boardMarkers[i].push(cell.getMark());
+            });
+        });
 
         // for the rows 
         function rowCheck() {
@@ -218,10 +217,9 @@ let gameController = (function() {
     }
     
     function playRound(position) {
-        displayController.displayBoard();
         // only for the console, I'spose
         let board = gameboard.getBoard();
-        let positions = displayController.getPositions();
+        let positions = gameboard.getPositions();
         let rowNumber;
         let cellNumber;
         positions.forEach(row => {
@@ -231,20 +229,18 @@ let gameController = (function() {
             }
         });
 
-        console.log(`${rowNumber}, ${cellNumber}`)
-        console.log(board)
         let cell = board[rowNumber][cellNumber];
 
         if (!cell.isMarked()) {
             cell.setMark(activePlayer.getMarker());
             
-    
             // check to see the winner
             if (checkWinner()){
                 console.log(`Winner: ${activePlayer.getName()}`)
             };
-    
+            
             displayController.displayBoard();
+
             changeActivePlayer();
         }
         console.log(`Turn: ${activePlayer.getName()} Marker: ${activePlayer.getMarker()}`);
@@ -253,55 +249,8 @@ let gameController = (function() {
     // and everything begins with playGame
     function playGame() {
         let count = 0;
-        let position;
         gameboard.createBoard();
         displayController.swapButtons();
-        
-        while(true && count < 9) {
-            do {
-                displayController.displayPositions();
-                
-                // TODO: prompt is not allowing the displayBoard to display the content immediately
-                position = Number(prompt("Position (one from the console): "))
-                
-                // if user presses cancel, prompt returns 0
-                if (position === 0) {
-                    break;
-                }
-            } while (position > 9 || position < 1 || (position === ''));
-            if (position) {
-                gameController.playRound(position);
-                
-                // activePlayer gets changed every round at the end, so if you want to checkWinner for this, round
-                // you have to do this
-                gameController.changeActivePlayer()
-                if (gameController.checkWinner()) {
-                    break;
-                } 
-                
-                // count the number of marked cells each time
-                count = 0;
-                gameboard.getBoard().forEach(row => {
-                    row.forEach(cell => {
-                        if (cell.isMarked()) {
-                            count++;
-                        }
-                    })
-                })
-                gameController.changeActivePlayer()
-                
-            } else { /* if position null dincha bhane */
-                break;
-            }
-        }
-        
-        gameController.changeActivePlayer();
-        if (count === 9 && !gameController.checkWinner()) {
-            console.log("That was a draw!")
-        }
-        // if we've already broken out of the game, no need to change the active player again
-
-        console.log("Thanks for playing");
     }
     
     return {playRound, checkWinner, changeActivePlayer, playGame};
@@ -315,5 +264,14 @@ startButton.addEventListener('click', () => {
 resetButton.addEventListener('click', () => {
     gameboard.resetBoard();
 })
+
+ticTacBoard.addEventListener('click', (e) => {
+    let markerContainers = Array.from(ticTacBoard.querySelectorAll('.marker-container'));
+    let markerContainer = e.target;
+    if (markerContainers.includes(markerContainer)) {
+        let id = markerContainer.getAttribute('id');
+        gameController.playRound(+ id);
+    }
+});
 //       asking them for the replay isn't necessary cause we'll do that with the ui
 // i can't change the position of changeactiveplayer() yet, but in the future, will do
