01da6db38d9b6f2741806754ad7234ea9f7318d8
srj-17 <saugatrijal17@gmail.com>
2024-08-10 21:08:24 +0545

    Fix the dialog display bug
    Caused by display: grid overriding the default dialog
    display property

--- script.js
+++ script.js
@@ -128,7 +128,14 @@ let displayController = (function() {
                 if (!cell.isMarked()) {
                     markerContainer.textContent = ' ';
                 } else {
-                    markerContainer.textContent = cell.getMark();
+                    mark = cell.getMark();
+                    if (mark === 'x') {
+                        markerContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                                                    </svg>`
+                    } else {
+                        markerContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+                                                    </svg>`
+                    }
                 }
                 ticTacBoard.appendChild(markerContainer);
             })
@@ -295,8 +302,8 @@ let gameController = (function() {
             checkWinnerCount++;
             if (checkWinner()){
                 displayController.displayWinner(activePlayer);
+                console.log('working')
             };
-
             
             displayController.displayBoard();
             
@@ -348,3 +355,5 @@ winnerDialog.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
     buttonsContainer.appendChild(startButton);
 })
+
+// TODO: winnerDialog is not closing, unexpectedly, idk what's wrong
